ec7641d6fa5d88d77a3b8b7fd2132f4f0f717bde
Yared Gudeta <yaredgudeta@DC-YGUDETA-OSX.fios-router.home>
2018-07-05 16:11:54 -0400

    Added invalid test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -9,7 +9,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello Maven");
+        runFlowAndExpect("mainFlow", "Flow 1");
     }
     
     @Override
